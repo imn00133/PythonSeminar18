68ec9e9db12969a66198c42d776e12d87e32d157
Kim Jae Hyeong <imn00133@naver.com>
2018-12-08 00:13:31 +0000

    add function to type people number in lottery.py

--- Exercise/Lecturer/lottery/lottery.py
+++ Exercise/Lecturer/lottery/lottery.py
@@ -4,10 +4,11 @@ people_list = []
 with open('people.txt', 'r', encoding='utf-8') as file:
     people_list = file.readlines()
 
+lottery_num = int(input("추첨할 인원을 적어주세요: "))
 with open('winner.txt', 'w', encoding='utf-8') as file:
     file.write("당첨\n")
-    for i in range(6):
-        file.write(people_list.pop(random.randint(0, len(people_list))))
+    for i in range(lottery_num):
+        file.write(people_list.pop(random.randint(0, len(people_list)-1)))
     file.write("낙첨, 다음기회에\n")
     for people in people_list:
         file.write(people)
