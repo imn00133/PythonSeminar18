2d08e3165fdb9c07c0aaa650e8c7304daa46b6d8
Jaehyeong Kim <imn00133@gmail.com>
2018-12-08 00:09:38 +0000

    rename directory to match convention

--- Exercise/Lecturer/Lottery/Lottery.py
+++ Exercise/Lecturer/Lottery/Lottery.py
@@ -1,10 +1,10 @@
 import random
 
 people_list = []
-with open('people.txt', 'r', encoding='utf-8') as file:
+with open('People.txt', 'r', encoding='utf-8') as file:
     people_list = file.readlines()
 
-with open('winner.txt', 'w', encoding='utf-8') as file:
+with open('Winner.txt', 'w', encoding='utf-8') as file:
     file.write("당첨\n")
     for i in range(6):
         file.write(people_list.pop(random.randint(0, len(people_list))))
