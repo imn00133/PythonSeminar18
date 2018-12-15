a62ae726a366977c6c8902cb010626d92c42c774
Kim Jae Hyeong <imn00133@naver.com>
2018-12-15 08:24:51 +0000

    rename directory and file name

--- Exercise/Lecturer/lottery/lottery.py
+++ Exercise/Lecturer/lottery/lottery.py
@@ -1,11 +1,11 @@
 import random
 
 people_list = []
-with open('People.txt', 'r', encoding='utf-8') as file:
+with open('people.txt', 'r', encoding='utf-8') as file:
     people_list = file.readlines()
 
 lottery_num = int(input("추첨할 인원을 적어주세요: "))
-with open('Winner.txt', 'w', encoding='utf-8') as file:
+with open('winner.txt', 'w', encoding='utf-8') as file:
     file.write("당첨\n")
     for i in range(lottery_num):
         file.write(people_list.pop(random.randint(0, len(people_list)-1)))
