5df86e76755e1a40d91f420bcd915f57eaa06a46
Kim Jae Hyeong <imn00133@naver.com>
2018-12-16 11:21:45 +0000

    add 18 lecture file to use lecture's files of presentation

---
+++ TeachingMaterials/insert_file/18_lecture_file/overlapping_dictionary.py
@@ -0,0 +1,14 @@
+terrestrial_planet = {
+    'Mercury': {
+        'mean_radius': 2439.7,
+        'mass': 3.3022E+23,
+        'orbital_period': 87.969
+    },
+    'Venus': {
+        'mean_radius': 6051.8,
+        'mass': 4.8676E+24,
+        'orbital_period': 224.70069,
+    }
+}
+
+print(terrestrial_planet['Mercury']['mass'])
