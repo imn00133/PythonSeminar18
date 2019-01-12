30b2c8db451a60e5b4d023cd925428cac8100e53
Kim Jae Hyeong <imn00133@naver.com>
2019-01-12 01:17:32 +0000

    Seonhyeok submitted his third exercise

--- Users/Seonhyeok/BMI_calculator/BMI_calculator.py
+++ Users/Seonhyeok/BMI_calculator/BMI_calculator.py
@@ -1,15 +1,16 @@
+bmi_num = (18.5, 25, 30, 35)
 height = float(input("키를 입력하세요(m) : "))
 weight = float(input("몸무게를 입력하세요(kg) : "))
 
 bmi = weight / (height ** 2)
 
-if bmi < 18.5:
+if bmi < bmi_num[0]:
     print("저체중 입니다.")
-elif 18.5 <= bmi < 25:
+elif bmi_num[0] <= bmi < bmi_num[1]:
     print("정상체중 입니다.")
-elif 25 <= bmi < 30:
+elif bmi_num[1] <= bmi < bmi_num[2]:
     print("과체중 입니다.")
-elif 30 <= bmi < 35:
+elif bmi_num[2] <= bmi < bmi_num[3]:
     print("경도비만 입니다.")
-elif 35 <= bmi:
+elif bmi_num[3] <= bmi:
     print("고도비만 입니다.")
--- Users/Seonhyeok/vending_machine/vending_machine.py
+++ Users/Seonhyeok/vending_machine/vending_machine.py
@@ -2,8 +2,40 @@ money = 0
 input_money = input("돈을 넣으세요 : ")
 money += int(input_money)
 
-print("""1. 블랙커피(100원)
-2. 밀크커피(150원)
-3. 고급커피(200원)
+drink = [['블랙커피', 100], ['밀크커피', 150], ['고급커피', 200]]
+drink_num = int(len(drink))
+
+print("""1. %s(%d원)
+2. %s(%d원)
+3. %s(%d원)
 4. 거스름돈
-넣은 돈: %d원""" % money)
+넣은 돈: %d원""" % (drink[0][0], drink[0][1], drink[1][0],
+                drink[1][1], drink[2][0], drink[2][1], money))
+menu_num = int(input("뽑을 물품을 골라주세요 : "))
+
+if menu_num == 1:
+    if drink[0][1] <= money:
+        print("%s가 나왔습니다." % drink[0][0])
+        money -= drink[0][1]
+        print("돈을 반환합니다 : %d원" % money)
+    else:
+        print("""돈이 부족합니다.\n돈을 반환합니다 : %d원""" % money)
+elif menu_num == 2:
+    if drink[1][1] <= money:
+        print("%s가 나왔습니다." % drink[1][0])
+        money -= drink[1][1]
+        print("돈을 반환합니다 : %d원" % money)
+    else:
+        print("""돈이 부족합니다.\n돈을 반환합니다 : %d원""" % money)
+elif menu_num == 3:
+    if drink[2][1] <= money:
+        print("%s가 나왔습니다." % drink[2][0])
+        money -= drink[2][1]
+        print("돈을 반환합니다 : %d원" % money)
+    else:
+        print("""돈이 부족합니다.\n돈을 반환합니다 : %d원""" % money)
+elif menu_num == 4:
+    print("거스름돈을 반환합니다 : %d원" % money)
+elif not 1 <= menu_num <= drink_num+1:
+    print("""물품번호를 잘못 입력하셨습니다.
+돈을 반환합니다 : %d원""" % money)
