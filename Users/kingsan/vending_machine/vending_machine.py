a18236dce8aa0fcac856e32c473503fd5ee52ef9
Kim Jae Hyeong <imn00133@naver.com>
2019-01-23 09:32:49 +0000

    kingsan submitted his vending_machine.py

--- Users/kingsan/vending_machine/vending_machine.py
+++ Users/kingsan/vending_machine/vending_machine.py
@@ -1,47 +1,49 @@
-drink = {}
-drink['ame'] = {'name': "아메리카노", 'price': 3500}
-drink['sch'] = {'name': "딸기치즈케익할라치노", 'price': 5500}
-drink['gtl'] = {'name': "그린티라떼", 'price': 4500}
-drink['cbr'] = {'name': "콜드브루", 'price': 4000}
+def init(drinks_dic, c_money, mflag):
+    print_menu(drinks_dic)
+    if c_money != 0:
+        print("현재까지 넣은 돈은 %d원입니다.")
+    order_init = input("뽑을 물품을 골라주세요 : ")
+    order(order_init, drinks_dic, c_money, mflag)
 
-keys = 'ame', 'sch', 'gtl', 'cbr'
-flag = True
-input_money = 0
-current_money = 0
-min_price = min(drink[keys[0]]['price'],
-                drink[keys[1]]['price'],
-                drink[keys[2]]['price'],
-                drink[keys[3]]['price'])
 
-while flag:
-
-    print("\n1. 아메리카노(3500원)")
-    print("2. 딸기치즈케익할라치노(5500원)")
-    print("3. 그린티라떼(4500원)")
-    print("4. 콜드브루(4000원)")
-    print("5. 돈 넣기")
-    print("6. 거스름돈")
-    print("넣은 돈 : %d원" % current_money)
-
-    order = int(input("뽑을 물품을 골라주세요: "))
-
-    if order > 0 and order <= 4:
-        if drink[keys[order - 1]]['price'] <= current_money:
-            print(drink[keys[order - 1]]['name'], "이/가 나왔습니다.")
-            current_money -= drink[keys[order - 1]]['price']
-            if current_money < min_price:
-                print(current_money, "원을 반환합니다.")
-                current_money = 0
+def print_menu(drinks_dic):
+    for i, name in enumerate(drinks_dic['name']+["돈 입력", "거스름돈", "종료"], 1):
+        if i <= len(drinks_dic['name']):
+            print("%s. %s %d원" % (i, name, drinks_dic['price'][i-1]))
         else:
-            print("돈이 부족합니다. 돈을 더 넣어주세요.")
-    elif order == 5:
-        input_money = int(input("돈을 넣으세요: "))
-        current_money += input_money
-    elif order == 6:
-        print(current_money, "원을 반환합니다.")
-        current_money = 0
-    else:
-        flag = False
-        print(current_money, "원을 반환합니다.")
-        print("프로그램을 종료합니다.")
-        current_money = 0
+            print("%s. %s" % (i, name))
+
+
+def input_money(c_money):
+    inputmoney = int(input("돈을 넣어주십시오 : "))
+    return c_money + inputmoney
+
+
+def order(order_byuser, drinks_dic, c_money, mflag):
+    if '0' < order_byuser and order_byuser < '9':
+        print("인티저개꿀 %s" % order_byuser)
+        if 1 < order_byuser < 5:
+            pass
+        elif order_byuser == 5:
+            c_money = input_money(c_money)
+        elif order_byuser == 6:
+            mflag = False
+    elif order_byuser == "kingsan":
+            access_admin(drinks_dic)
+
+
+def access_admin(drinks_dic):
+    pass
+
+
+drinks = {}
+drinks['name'] = ["아메리카노", "야메리카노", "어메리카노", "여메리카노"]
+drinks['price'] = [3500, 4000, 4500, 5000]
+drinks['stock'] = [1, 2, 3, 4]
+
+current_money = 0
+flag = True
+while flag:
+    init(drinks, current_money, flag)
+
+    break
