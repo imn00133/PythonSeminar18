12a8b69af065dd9a4143bc416fb83c4b10e2a175
Kim Jae Hyeong <imn00133@naver.com>
2019-01-23 09:31:58 +0000

    Gihun changed his directory and submit vending_machine exercise

--- Users/Gihun/vending_machine/vending_machine.py
+++ Users/Gihun/vending_machine/vending_machine.py
@@ -1,3 +1,7 @@
+#상품 정의
+products = {'말차':500
+           '녹차':600
+           '라떼':650}
 money = input("돈 넣으쇼:")
 while not money.isdecimal():
     money = input("다시 넣으쇼:")
@@ -7,3 +11,5 @@ print("""1. 카페라떼(100원)
 3. 오라떼(200원)
 4. 거스름돈""")
 print("넣은 돈: {money}".format(money=money))
+
+selectnum = input("상품 번호를 입력하시게:")
