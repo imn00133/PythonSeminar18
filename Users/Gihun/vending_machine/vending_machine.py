b0e48abd1bbfeb1cbf759e4b16935aded8ae7077
Kim Jae Hyeong <imn00133@naver.com>
2019-01-24 09:38:12 +0000

    Gihun submit his exercise

--- Users/Gihun/vending_machine/vending_machine.py
+++ Users/Gihun/vending_machine/vending_machine.py
@@ -1,15 +1,26 @@
-#상품 정의
-products = {'말차':500
-           '녹차':600
-           '라떼':650}
+# 상품 정의
+products = list()
+products.append(['감자', 300])
+products.append(['고구마', 450])
+products.append(['건포도', 500])
+products.append(['히오스', 550])
+
+
 money = input("돈 넣으쇼:")
 while not money.isdecimal():
     money = input("다시 넣으쇼:")
 
-print("""1. 카페라떼(100원)
-2. 녹차라떼(150원)
-3. 오라떼(200원)
-4. 거스름돈""")
-print("넣은 돈: {money}".format(money=money))
 
-selectnum = input("상품 번호를 입력하시게:")
+pro_cnt = len(products)
+# 상품 개수
+
+menu = 0
+while pro_cnt > menu:
+    print('%d. %s: %d원' % (menu, products[menu][0], products[menu][1]))
+    menu = menu + 1
+print('%d. 거스름돈' % menu)
+
+select = input("상품 번호를 쓰시게")
+while not select.isnumeric():
+    select = input("다시 쓰쇼:")
+
