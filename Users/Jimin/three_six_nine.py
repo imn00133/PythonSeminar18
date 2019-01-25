ae0c1b469e9b4695d0740dbb8d42166a67fe6114
Kim Jae Hyeong <imn00133@naver.com>
2019-01-25 09:45:19 +0000

    Jimin submitted his forth exercise

--- Users/Jimin/three_six_nine.py
+++ Users/Jimin/three_six_nine.py
@@ -1,5 +1,5 @@
 
-def isMultiple3(num): # num을 int로 받기! , 3의 배수인지 확인해 출력하기 return 안한다 뛰어쓰기는 나중에 하자
+def isMultiple3(num):
     i = 1
     while i <= num:
         if i % 3 != 0:
@@ -12,15 +12,16 @@ def position(num):
     while num / 10 != 0:
         i += 1
     arr = []
-	j = 0
-	while j <= i:
-        arr.insert(0,num % 10)
+    j = 0
+    while j <= i:
+        arr.insert(0, num % 10)
         num /= 10
-	return arr
+    return arr
 
 def s6g(num):
     arr = position(num)
-    
+    for num2 in arr:
+        isMultiple3(num2) 
 
 num = int(input("마지막 number를 입력하숑 : "))
 s6g(num)
