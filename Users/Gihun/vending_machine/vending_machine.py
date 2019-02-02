8ab3b61061bb570fe778c291fd9c0be7c403761c
Kim Jae Hyeong <imn00133@naver.com>
2019-02-02 09:46:29 +0000

    Gihun submitted vending_machine exercise

--- Users/Gihun/vending_machine/vending_machine.py
+++ Users/Gihun/vending_machine/vending_machine.py
@@ -18,23 +18,32 @@ while True:
         print('%d. %s: %d원' % (menu+1, products[menu][0], products[menu][1]))
         menu = menu + 1
 
+    print(menu+1, ". 거스름돈")
     menu = menu + 1
-    print('%d. 거스름돈' % menu)
+    print(menu+1, ". 돈 추가")
+    menu = menu + 1
+    print(menu+1, ". 종료")
     print('넣은 돈 : %d 원 ' % money)
 
     # 상품 번호 확인
     select = int(input("상품 번호를 쓰시게 "))-1
 
     if 0 <= select < len(products):
-        if money > int(products[select][1]):
+        if money >= int(products[select][1]):
             money = money - products[select][1]
             print('%s 구매 완료' % (products[select][0]))
-            print('%d 원 반환' % (money))
-            break
+            print('%d 원 반환' % money)
         else:
             print("돈 부족")
+    elif select == (menu-2):
+        print('%d 원 반환' % money)
+        break
     elif select == menu-1:
-            print('%d 원 반환' % (money))
-            break
+        money_plus = int(input("돈 더 주쇼:"))
+        money += money_plus
+    elif select == menu or select == -2:
+        print('%d 원 반환' % money)
+        print("종료합니다")
+        break
     else:
         print("잘못된 상품 번호다")
