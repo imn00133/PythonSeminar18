517dbb4651e69d95ad481ea019577716acac5f68
Kim Jae Hyeong <imn00133@naver.com>
2019-02-08 09:25:31 +0000

    Jun submitted his sixth exercise

--- Users/JUN/number_baseball/number_baseball.py
+++ Users/JUN/number_baseball/number_baseball.py
@@ -1,74 +1,67 @@
 from random import randrange
 
 
-def judgment(arr, n):
-    for j in arr:
-        if n == j:
+def che(arr, n):
+    for i in arr:
+        if i == n:
             return False
     return True
 
 
-def makenum(computer):
+def count(com, use):
+    scou = 0
+    bcou = 0
     for i in range(0, 4):
-        n = randrange(10)
-        if judgment(computer, n):
-            computer.append(n)
-    for i in computer:
-        print("%d" % i, end='')
-    print("")
-
-
-def ball_judge(user, computer):
-    strike = 0
-    ball = 0
-    out = 0
-    for i in range(0, 4):
-        out = out+1
         for j in range(0, 4):
-            print("%d %d" % (user[j], computer[i]))
-            if user[j] == computer[i]:
+            if use[i] == com[j]:
                 if i == j:
-                    strike = strike+1
+                    scou = scou+1
                 else:
-                    ball = ball+1
-                out = out-1
-                break
-    print("strike:%d ball:%d out:%d" % (strike, ball, out))
-    if strike == 4:
+                    bcou = bcou+1
+    print("strike:%d ball:%d out:%d" %(scou, bcou, 4-(scou+bcou)))
+    if scou == 4:
         return True
     return False
 
 
-def conti():
+def option():
     while True:
-        con = (str)(input("계속하시겠습니까?(yes/no): "))
-        if cou == "yes" or cou == "y":
+        op = (str)(input("계속?(yes/no):"))
+        if op == "yes" or op == "y":
             return False
-        elif cou == "no" or cou == "n":
+        elif op == "no" or op == "n":
             return True
 
 
-computer = list()
-user = list()
 while True:
     cou = 0
-    makenum(computer)
+    computer = list()
+    while len(computer) < 4:
+        n = (str)(randrange(0, 9))
+        if che(computer, n):
+            computer += n
     while True:
-        q = 1000
-        n = (int)(input("입력(0000~9999): "))
-        for i in range(0, 4):
-            if judgment(user, n/q) and n/q >= 0 and n/q <= 9:
-                user.insert(i, n/q)
-                n = n % q
-                q = q/10
-            else:
-                break
-        if i != 3:
-            cou = cou-1
-            print("다시 입력해주세요")
+        cou = cou+1
+        user = (str)(input("입력(0000~9999): "))
+        if len(user) > 4:
+            print("큼")
             continue
-        if ball_judge(user, computer):
-            print("%d만에 성공하셨습니다." % cou)
+        elif len(user) < 4:
+            print("작음")
+            continue
+        for i in range(0, 4):
+            for j in range(i+1, 4):
+                if che(user[i], user[j]) == False:
+                    break
+            if j < 3:
+                break
+        if i < 3:
+            cou = cou-1
+            print("중복X")
+            continue
+        if count(computer, user):
+            print("%d만에 성공" %cou)
             break
-    if conti():
+    if option():
+        print("종료합니다.")
         break
--- Users/JUN/vending_machine/vending_machine.py
+++ Users/JUN/vending_machine/vending_machine.py
@@ -3,6 +3,16 @@ import sys
 from time import sleep
 
 
+def save(moolgun_dic, moolgun_list, moolgun_value):
+    db = open("moolgundb.txt", 'w')
+    for i in range(0, len(moolgun_list)):
+        arr = moolgun_list[i] + " "
+        arr += (str)(moolgun_value[i]) + " "
+        arr += (str)(moolgun_dic.get(moolgun_list[i])[1]) + "\n"
+        db.write(arr)
+    db.close()
+
+
 def bujok():
     print("저런저런 돈이 부족하다구 친구")
 
@@ -14,67 +24,86 @@ def Dche(Don):
         print("%d원 반환" % Don)
 
 
-def admin_list(moolgun, moolgun_cou):
+def admin_list(moolgun_dic):
     i = 1
-    for mo in moolgun:
-        print("%d. %s: %d" % (i, mo, moolgun_cou[mo]))
+    for mo in moolgun_dic.keys():
+        print("%d. %s: %d" % (i, mo, moolgun_dic[mo][1]))
         i += 1
 
 
-def admin_mode(moolgun, moolgun_cou):
+def admin_mode(moolgun_dic, moolgun_list, moolgun_value):
     while True:
         print("1. 물품 목록과 ㄱㅐ수 출력")
         print("2. 물품ㄱㅐ수 변경")
         print("3. admin_mode 종료")
         select = (int)(input("입력하시게: "))
         if select == 1:
-            admin_list(moolgun, moolgun_cou)
+            admin_list(moolgun_dic)
         elif select == 2:
-            admin_list(moolgun, moolgun_cou)
+            admin_list(moolgun_dic)
             sel_num = (int)(input("뭘 바꾸실텐가?: "))
             cou = (int)(input("몇개?: "))
-            moolgun_cou[moolgun[sel_num-1]] = cou
+            moolgun_dic[moolgun_list[sel_num-1]][1] = cou
         elif select == 3:
             break
         else:
             print("Ang~?")
         sleep(1)
         os.system('clear')
+    save(moolgun_dic, moolgun_list, moolgun_value)
 
 
-moolgun_cou = {'블랙말랑카우': 5, '밀크커피': 5, '고급시계': 5}
-moolgun = ["블랙말랑카우", "밀크커피", "고급시계"]
-ga_guek = [100, 150, 39900]
+try:
+    db = open("moolgundb.txt", 'r')
+except No_DB:
+    print("관리자를 호출해주세요")
+moolgun_dic = dict()
+moolgun_value = list()
+moolgun_list = list()
+while True:
+    i = (str)(db.readline())
+    i = i.split()
+    if i == []:
+        break
+    moolgun_dic.setdefault(i[0], [(int)(i[1]), (int)(i[2])])
+db.close()
+for i in moolgun_dic.keys():
+    moolgun_list.append(i)
+for i in moolgun_list:
+    moolgun_value.append(moolgun_dic.get(i)[0])
 Don = (int)(input("초기배팅금액 입력:"))
 while True:
-    print("1. 블랙말랑카우(100원)")
-    print("2. 밀크커피(150원)")
-    print("3. 고급시계(39900원)")
+    coui = 1
+    for i in moolgun_dic.keys():
+        print("%d. %s(%s)" % (coui, i, moolgun_dic.get(i)[0]))
+        coui = coui+1
     print("4. 돈좀 넣으쉴?")
     print("5. 거스름돈")
     print("6. 종료")
     sel = input("선택: ")
     if sel == "admin":
         os.system('clear')
-        admin_mode(moolgun, moolgun_cou)
+        admin_mode(moolgun_dic, moolgun_list, moolgun_value)
     if sel > '0' and sel <= '9':
         sel = (int)(sel)
         if sel == 6:
             Dche(Don)
+            save(moolgun_dic, moolgun_list, moolgun_value)
             break
         elif sel < 4:
-            if Don < ga_guek[sel-1]:
+            if Don < moolgun_value[sel-1]:
                 bujok()
-            elif moolgun_cou.get(moolgun[sel-1]) > 0:
-                Don -= ga_guek[sel-1]
-                moolgun_cou[moolgun[sel-1]] -= 1
-                print("%s가 나왔습니다." % moolgun[sel-1])
+            elif moolgun_dic.get(moolgun_list[sel-1])[0] > 0:
+                Don -= moolgun_value[sel-1]
+                moolgun_dic[moolgun_list[sel-1]][1] -= 1
+                print("%s가 나왔습니다." % moolgun_list[sel-1])
             else:
                 print("물건이 부족합니다.")
         elif sel == 4:
             Don += (int)(input("감사합니다.: "))
-        if Don < min(ga_guek) or sel == 5:
+        if Don < min(moolgun_value) or sel == 5:
             Dche(Don)
+            save(moolgun_dic, moolgun_list, moolgun_value)
             Don = 0
     sleep(1)
     os.system('clear')
