306730d122bdd96a71991418a0eb83865454121b
Kim Jae Hyeong <imn00133@naver.com>
2019-02-08 09:24:29 +0000

    Escape submitted his exercise

--- Users/Escape/BMI_calculator2.py
+++ Users/Escape/BMI_calculator2.py
@@ -4,11 +4,13 @@ height = float(input('키를 입력해주세요(cm)\t\t: ')) * 0.01
 weight = float(input('몸무게를 입력해주세요(kg)\t: '))
 BMI = weight / height ** 2
 
+level_cut = (18.5, 23, 25, 30, 35)
+level_name = ('저체중', '정상', '과체중', '경도 비만', '중증도 비만', '고도 비만')
+
 # 저체중, 정상, 과체중, ... 순으로 각각 0, 1, 2, ...의 level을 부여한다.
 # 각각의 커트라인을 통과할 때마다 레벨 1이 상승하는 방식이다. 세상에서 가장 기분 나쁜 레벨업
-for i, x in enumerate((18.5, 23, 25, 30, 35)):
+for level, x in enumerate(level_cut):
     if BMI < x:
         break
 
-print('\nBMI는 %0.2f이고, %s에 해당됩니다.'
-      % (BMI, ('저체중', '정상', '과체중', '경도 비만', '중증도 비만', '고도 비만')[i]))
+print('\nBMI는 %0.2f이고, %s에 해당됩니다.' % (BMI, level_name[level]))
